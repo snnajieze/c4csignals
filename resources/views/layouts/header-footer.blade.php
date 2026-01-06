737be7cdef02286d5f9964c5665945e0a56e2f7e
nossh <nosshamary@gmail.com>
2026-01-06 01:58:49 +0100

    feat: Add Facebook Meta Pixel tracking code for analytics

--- resources/views/layouts/header-footer.blade.php
+++ resources/views/layouts/header-footer.blade.php
@@ -45,6 +45,20 @@
 
         @livewireStyles
 
+
+        <!-- Meta Pixel Code -->
+         <script>
+         !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '1411128380520414'); fbq('track', 'PageView');
+
+          @if(Route::currentRouteName() == 'nowpayment.success')
+              // Fire Purchase only on the checkout success page
+              fbq('track', 'Purchase', {value: 0.00, currency: 'USD'});
+          @endif
+
+        </script>
+        
+        <noscript> <img height="1" width="1" src="https://www.facebook.com/tr?id=1411128380520414&ev=PageView&noscript=1"/></noscript><!-- End Meta Pixel Code -->
+
     </head>
     <body class="items-center lg:justify-center min-h-screen flex-col">
         
